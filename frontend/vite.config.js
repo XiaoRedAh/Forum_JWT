14a6276281c3bed76b5e5904c8123acda79c3646
XiaoRedAh <2976698583@qq.com>
2023-12-03 14:41:37 +0800

    添加README.md

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server:{ host: '127.0.0.1', port: 3000 },
     plugins: [
         vue(),
         AutoImport({
